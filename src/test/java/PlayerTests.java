2b940350d70d617997c51b2e05a440e8d8bff87b
Vladimir <kesseloficcial@gmail.com>
2024-04-03 18:44:48 +0700

    Написал тест для проверки корректности получения числа попыток игрока

---
+++ src/test/java/PlayerTests.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PlayerTests {
+    private static final int PLAYER_ATTEMPTS = 5;
+    private static final Player PLAYER = new Player(PLAYER_ATTEMPTS);
+
+    @Test
+    public void GetAttemptsTest(){
+        assertEquals(PLAYER.getAttempts(), PLAYER_ATTEMPTS);
+    }
+}
