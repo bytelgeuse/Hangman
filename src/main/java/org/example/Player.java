359f4688413197aa403a3ab4a0cadb351c7254e2
Vladimir <kesseloficcial@gmail.com>
2024-04-03 18:52:51 +0700

    Минимальная реализация класса Player

---
+++ src/main/java/org/example/Player.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Player {
+    private int attempts;
+
+    public Player(int attempts){
+        this.attempts = attempts;
+    }
+
+    public int getAttempts() {
+        return attempts;
+    }
+}
--- src/test/java/PlayerTests.java
+++ src/test/java/PlayerTests.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import org.example.Player;
 public class PlayerTests {
     private static final int PLAYER_ATTEMPTS = 5;
     private static final Player PLAYER = new Player(PLAYER_ATTEMPTS);
