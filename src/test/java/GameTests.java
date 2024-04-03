5771197d481c96fa372e4021aa731d05ac1ce3ed
Vladimir <kesseloficcial@gmail.com>
2024-04-03 19:20:26 +0700

    Написал тест, проверяющий корректный вывод отгаданных букв

--- src/test/java/GameTests.java
+++ src/test/java/GameTests.java
@@ -1,13 +1,22 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.example.Game;
 public class GameTests {
     private static final Game GAME = new Game();
+    private static final String WORD = "Окошко";
 
     @Test
     public void InitGameTest(){
         assertNotNull(GAME.getPlayer());
         assertNotNull(GAME.getWordList());
+    }
+
+    @Test
+    public void GetHiddenWord(){
+        GAME.setCurrentWord(WORD);
+        GAME.addToGuessedLetters('к');
+        assertEquals(GAME.getHiddenWord(), "_к__к_");
     }
 }
