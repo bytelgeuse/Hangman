e4d21f1f2d429901c666d65b8b1f1492f6cd53bd
Vladimir <kesseloficcial@gmail.com>
2024-04-03 19:37:58 +0700

    Реализовал отображение отгаданных букв в загаданном слове

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -1,13 +1,24 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Game {
     private final Player player;
     private final WordList wordList;
+
+    private String currentWord;
+
+    private Set<Character> guessedLetters;
 
     public Game(){
         System.out.println("Инициализация игры \"Виселица\"...");
         player = new Player();
         wordList = new WordList();
+        guessedLetters = new HashSet<>();
+        currentWord = wordList.getRandom();
         System.out.println("Инициализация завершена");
     }
 
@@ -17,5 +28,36 @@ public class Game {
 
     public WordList getWordList() {
         return wordList;
+    }
+
+    public String getCurrentWord() {
+        return currentWord;
+    }
+
+    public void setCurrentWord(String currentWord) {
+        this.currentWord = currentWord;
+    }
+
+    public Set<Character> getGuessedLetters() {
+        return guessedLetters;
+    }
+
+    public void setGuessedLetters(Set<Character> guessedLetters) {
+        this.guessedLetters = guessedLetters;
+    }
+
+    public void addToGuessedLetters(char c){
+        this.guessedLetters.add(c);
+    }
+
+    public String getHiddenWord(){
+        StringBuilder current = new StringBuilder(this.currentWord);
+
+        for(int i = 0; i < current.length(); i++){
+            if(!guessedLetters.contains(current.charAt(i)))
+                current.replace(i,i+1,"_");
+        }
+
+        return current.toString();
     }
 }
